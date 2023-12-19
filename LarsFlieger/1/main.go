c4574b6028e5a5de70fa5f5931de94dd39438239
LarsFlieger <a.purenetwork@gmail.com>
2023-12-20 00:02:54 +0100

    feat: add 1a

---
+++ LarsFlieger/1/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	dat, e := os.ReadFile("./inputs/1")
+	if e != nil {
+		panic(e)
+	}
+	lines := strings.Split(string(dat), "\n")
+
+	var result int
+
+	// loop over each line
+	for _, line := range lines {
+		sum := 0
+		// find first digit, start from first char
+		for i := 0; i <= len(line)-1; i++ {
+			if unicode.IsDigit(rune(line[i])) {
+				sum += int(line[i]-'0') * 10 // multiply by 10 because first digit
+				break                        // stop loop if found
+			}
+		}
+
+		// find last digit, start from last char
+		for i := len(line) - 1; i >= 0; i-- {
+			if unicode.IsDigit(rune(line[i])) {
+				sum += int(line[i] - '0')
+				break // stop loop if found
+			}
+		}
+		// append to result
+		result += sum
+	}
+	fmt.Println(result)
+}
